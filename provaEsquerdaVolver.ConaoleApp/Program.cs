9a573b25ec0dcc83e782e2ee1926f3577150d6ca
lucasvs07 <lucasviniciusdasilva0@gmail.com>
2022-02-18 16:30:09 -0300

    implementando todos os requisitos

--- provaEsquerdaVolver.ConaoleApp/Program.cs
+++ provaEsquerdaVolver.ConaoleApp/Program.cs
@@ -6,55 +6,76 @@ namespace provaEsquerdaVolver.ConaoleApp
     {
         static void Main(string[] args)
         {
-            string sentido = "N";
+            char sentido = 'N';
+            
+            int quantidadeDeOrdens = 1;
+            string sentidoFinal = "";
+            char[] ordens = { };
 
-            Console.WriteLine("Quantas ordens o soldado deve executar?");
-            string strQuantidadeDeOrdens = Console.ReadLine();
-            int quantidadeDeOrdens = Convert.ToInt32(strQuantidadeDeOrdens);
-
-            Console.Write("Quais as ordens que devem ser executadas?\n" +
-                "Considere D para direita e E para esquerda.");
-            string strOrdens = Console.ReadLine();
-            char[] ordens = strOrdens.ToCharArray();
-
-            for (int i = 0; i < quantidadeDeOrdens; i++)
+            while (true)
             {
-                if (ordens [i] == 'D' && sentido == "N")
-                {
-                    sentido = "L";
-                }
-                else if (ordens[i] == 'E' && sentido == "N")
-                {
-                    sentido = "O";
-                }
-                else if (ordens[i] == 'D' && sentido == "L")
-                {
-                    sentido = "N";
-                }
-                else if (ordens[i] == 'E' && sentido == "L")
-                {
-                    sentido = "S";
-                }
-                else if (ordens[i] == 'D' && sentido == "S")
-                {
-                    sentido = "O";
-                }
-                else if (ordens[i] == 'E' && sentido == "S")
-                {
-                    sentido = "L";
-                }
-                else if (ordens[i] == 'D' && sentido == "O")
-                {
-                    sentido = "N";
-                }
-                else if (ordens[i] == 'E' && sentido == "O")
-                {
-                    sentido = "S";
-                }
+                
+                Console.WriteLine("Quantas ordens o soldado deve executar?");
+                string strQuantidadeDeOrdens = Console.ReadLine();
+                quantidadeDeOrdens = Convert.ToInt32(strQuantidadeDeOrdens);
+                char[] qtdordens = new char[quantidadeDeOrdens];
 
+                if (quantidadeDeOrdens == 0)
+                    break;
+                else
+                {
+                    for (int j = 1; j <= quantidadeDeOrdens; j++)
+                    {
+                        Console.Write("Quais as ordens que devem ser executadas?\n" +
+                            "Considere D para direita e E para esquerda.\n");
+                        string strOrdens = Console.ReadLine();
+                        string strOrdensTotal = "" + strOrdens;
+                        ordens = strOrdensTotal.ToCharArray();
+
+                    }
+
+
+                        for (int i = 1; i <= quantidadeDeOrdens; i++)
+                        {
+                            if (ordens[i] == 'D' && sentido == 'N')
+                            {
+                                sentido = 'L';
+                            }
+                            else if (ordens[i] == 'E' && sentido == 'N')
+                            {
+                                sentido = 'O';
+                            }
+                            else if (ordens[i] == 'D' && sentido == 'L')
+                            {
+                                sentido = 'N';
+                            }
+                            else if (ordens[i] == 'E' && sentido == 'L')
+                            {
+                                sentido = 'S';
+                            }
+                            else if (ordens[i] == 'D' && sentido == 'S')
+                            {
+                                sentido = 'O';
+                            }
+                            else if (ordens[i] == 'E' && sentido == 'S')
+                            {
+                                sentido = 'L';
+                            }
+                            else if (ordens[i] == 'D' && sentido == 'O')
+                            {
+                                sentido = 'N';
+                            }
+                            else if (ordens[i] == 'E' && sentido == 'O')
+                            {
+                                sentido = 'S';
+                            }
+
+                        }
+                    
+                    sentidoFinal = "\n" + sentido;
+                }
             }
-
-            Console.WriteLine(sentido);
+            Console.WriteLine(sentidoFinal);
         }
     }
 }
