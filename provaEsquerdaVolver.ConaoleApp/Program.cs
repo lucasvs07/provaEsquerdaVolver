9c2fb931c6c01a3dc83fb56e44eda87f443ff092
lucasvs07 <lucasviniciusdasilva0@gmail.com>
2022-02-18 14:32:30 -0300

    Add project files.

---
+++ provaEsquerdaVolver.ConaoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace provaEsquerdaVolver.ConaoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentido = "N";
+
+            Console.WriteLine("Quantas ordens o soldado deve executar?");
+            string strQuantidadeDeOrdens = Console.ReadLine();
+            int quantidadeDeOrdens = Convert.ToInt32(strQuantidadeDeOrdens);
+
+            Console.Write("Quais as ordens que devem ser executadas?\n" +
+                "Considere D para direita e E para esquerda.");
+            string strOrdens = Console.ReadLine();
+            char[] ordens = strOrdens.ToCharArray();
+
+            for (int i = 0; i < quantidadeDeOrdens; i++)
+            {
+                if (ordens [i] == 'D' && sentido == "N")
+                {
+                    sentido = "L";
+                }
+                else if (ordens[i] == 'E' && sentido == "N")
+                {
+                    sentido = "O";
+                }
+                else if (ordens[i] == 'D' && sentido == "L")
+                {
+                    sentido = "N";
+                }
+                else if (ordens[i] == 'E' && sentido == "L")
+                {
+                    sentido = "S";
+                }
+                else if (ordens[i] == 'D' && sentido == "S")
+                {
+                    sentido = "O";
+                }
+                else if (ordens[i] == 'E' && sentido == "S")
+                {
+                    sentido = "L";
+                }
+                else if (ordens[i] == 'D' && sentido == "O")
+                {
+                    sentido = "N";
+                }
+                else if (ordens[i] == 'E' && sentido == "O")
+                {
+                    sentido = "S";
+                }
+
+            }
+
+            Console.WriteLine(sentido);
+        }
+    }
+}
